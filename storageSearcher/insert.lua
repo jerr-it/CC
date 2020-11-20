a66f2972d65bec7cfbb172fc03cb2cab3efe705c
Jerrit Gläsker <50867655+cherrysrc@users.noreply.github.com>
2020-11-20 22:52:24 +0100

    Added insert

---
+++ storageSearcher/insert.lua
@@ -0,0 +1,23 @@
+os.loadAPI("customAPI/config.lua")
+config.load(".config")
+self = config.get("self")
+
+local slotCount = 16
+
+function insert(slot)
+    local devices = peripheral.getNames()
+    for _,device in pairs(devices) do
+        if string.find(device, "chest") then
+            local chest = peripheral.wrap(device)
+        
+            chest.pullItems(self, slot, 64)
+            if turtle.getItemCount(slot) == 0 then
+                return
+            end
+        end
+    end
+end
+
+for i = 1,slotCount,1 do
+    insert(i)
+end
