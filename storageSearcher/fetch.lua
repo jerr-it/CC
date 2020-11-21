34f84719c662ef17cf08544a4c6a86a14dc68a65
Jerrit Gläsker <50867655+cherrysrc@users.noreply.github.com>
2020-11-21 13:18:07 +0100

    Fixed Fetch

--- storageSearcher/fetch.lua
+++ storageSearcher/fetch.lua
@@ -1,8 +1,8 @@
 shell.run("delete customAPI/config.lua")
-shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/master/config.lua customAPI/config.lua")
+shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/config.lua customAPI/config.lua")
 
 shell.run("delete get.lua")
-shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/master/get.lua get.lua")
+shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/get.lua get.lua")
 
 shell.run("delete insert.lua")
-shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/master/insert.lua insert.lua")
+shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/insert.lua insert.lua")
