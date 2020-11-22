c45ec39a56ad1c3e07bf3f73adcabf22cb175aa6
Jerrit Gläsker <50867655+cherrysrc@users.noreply.github.com>
2020-11-22 21:18:45 +0100

    Small fix

--- config/fetch.lua
+++ config/fetch.lua
@@ -2,10 +2,10 @@
 
 --Modules own files
 shell.run("delete config.lua")
-shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/config.lua config.lua")
+shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/config/config.lua config.lua")
 
 shell.run("delete setvar.lua")
-shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/setvar.lua setvar.lua")
+shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/config/setvar.lua setvar.lua")
 
 shell.run("delete create.lua")
-shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearcher/setvar.lua create.lua")
+shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/config/setvar.lua create.lua")
