8e91bf6a40ca25d18f91c08d5d8efd5b7d9511d2
Jerrit Gläsker <50867655+cherrysrc@users.noreply.github.com>
2020-11-25 23:04:43 +0100

    Usability Update

--- storageSearcher/fetch.lua
+++ storageSearcher/fetch.lua
@@ -18,5 +18,8 @@ shell.run("wget https://raw.githubusercontent.com/cherrysrc/CC/main/storageSearc
 
 --Initial actions, initializing a config file, running initialisation on Dependencies...
 shell.run("/config/create storageConfig")
-print("cd into storageSearcher directory and run init for first time use")
-print("Also you should run init to update your connected chests")
+print("---------------------")
+print("*** 1. Switch into the storageSearcher directory using 'cd storageSearcher' ***")
+print("*** 2. Run 'init <name>' to setup your storageSearcher ***")
+print("*** PS. Works only on turtles ***")
+print("*** PS. You can find the <name> by attaching a modem to your turtle and right clicking it ***")
